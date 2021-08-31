03da52ab0210d8bcbcd8d55279f3f4d2b3d50275
Luke Vo <lukevo1101@gmail.com>
2021-08-31 22:02:02 +0700

    Update fake_data.dart

--- food_app/lib/models/fake_data.dart
+++ food_app/lib/models/fake_data.dart
@@ -7,7 +7,7 @@ final FAKE_DATA = [
   Category(id: 2, content: 'Pizza', color: Colors.teal),
   Category(id: 3, content: 'Apple', color: Colors.pink),
   Category(id: 4, content: 'Vietnam', color: Colors.purpleAccent),
-  Category(id: 5, content: 'Pho', color: Colors.deepPurpleAccent),
+  Category(id: 5, content: 'Ph2o', color: Colors.deepPurpleAccent),
   Category(id: 6, content: 'Banh Mi', color: Colors.redAccent),
   Category(id: 1, content: 'Japanese', color: Colors.deepOrange),
   Category(id: 2, content: 'Pizza', color: Colors.teal),
